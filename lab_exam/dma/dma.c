3cb5dd75497e11e4bdfb68a3145a326c10b0586a
“Bigyan <bigyansanjyal56@gmail.com>
2025-02-13 19:24:37 +0545

    modify

--- lab_exam/dma/dma.c
+++ lab_exam/dma/dma.c
@@ -1,26 +1,19 @@
 #include<stdio.h>
-#include<stdlib.h>
-int main()
-
-{
-    int *ptr, i, n;
-    printf("enter the size of an array:");
-    scanf("%d",&n);
-    ptr = (int*) calloc (n, sizeof(int));
-    if(ptr == NULL)
-    {
-        return 0;
-    }
-    for (i = 0; i<n; i++)
-    {
-        printf("enter telement %d:", i+1);
-        scanf("%d", ptr+i);
-    }
-    printf("the elements of the array are:");
-    for(i = 0; i<n; i++)
-    {
-        printf("%d\n", *(ptr+i));
-    }
-    free(ptr);
-    return 0;
+int main(){
+int a[100], size, i;
+printf("Enter the size of array:");
+scanf("%d",&size);
+//input array
+for(i=0;i<size;i++){
+printf("Enter a[%d] element:",i);
+scanf("%d",&a[i]);
+}
+//output array at even index
+printf("The array elements are:\n");
+for(i=0;i<size;i++){
+if(i%2==0){
+printf("\n%d\n",a[i]);
+}
+}
+return 0;
 }
