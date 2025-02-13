c6af605e420e4297da0d5ee971375a184bf04d9f
“Bigyan <bigyansanjyal56@gmail.com>
2025-02-13 19:31:11 +0545

    modify

--- lab_exam/dma/dma.c
+++ lab_exam/dma/dma.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-int a[100], size, i;
+int a[100], size, i, max, min;
 printf("Enter the size of array:");
 scanf("%d",&size);
 //input array
@@ -11,9 +11,19 @@ scanf("%d",&a[i]);
 //output array at even index
 printf("The array elements are:\n");
 for(i=0;i<size;i++){
-if(i%2==0){
 printf("\n%d\n",a[i]);
 }
+max = a[0];
+min = a[0];
+for(i=1;i<size;i++){
+    if(a[i]>max){
+        max = a[i];
+    }
+    if(a[i]<min){
+        min = a[i];
+    }
 }
+printf("The maximum element in an array is %d\n",max);
+printf("The minimum element in an array is %d\n",min);
 return 0;
 }
