daa0a142c949e4c72d17d7bee8f40bcc71edf17f
Bigyan Sanjyal <bigyansanjyal56@gmail.com>
2025-04-26 21:50:53 +0545

    modify

--- OOP using C++/tutorial1.cpp
+++ OOP using C++/tutorial1.cpp
@@ -100,53 +100,141 @@
 //     return 0;
 // }
 
-//Defunung a member function inside a class
-#include <iostream>
-#include <string>
-using namespace std;
-class Student
-{
-    private:
-        string name;
-        int rollno;
-        int marks;
-    public:
-        void getDetails()
-        {
-            cout:"Enter full name of student: ";
-            getline(cin, name);
-            cout << "enter roll number of student: ";
-            cin >> rollno;
-            cout << "enter marks of student: ";
-            cin >> marks;
-        }
-        void showDetails()
-        {
-            cout << "Name of student is: " << name << endl;
-            cout << "Roll number of student is: " << rollno << endl;
-            cout << "Marks of student is: " << marks << endl;
-        }
-        void showGrade()
-        {
-            if (marks >= 90)
-                cout << "Grade of student is: A+" << endl;
-            else if (marks >= 80 && marks < 90)
-                cout << "Grade of student is: A" << endl;
-            else if (marks >= 70 && marks < 80)
-                cout << "Grade of student is: B" << endl;
-            else if (marks >= 60 && marks < 70)
-                cout << "Grade of student is: C" << endl;
-            else if (marks >= 50 && marks < 60)
-                cout << "Grade of student is: D" << endl;
-            else
-                cout << "Grade of student is: F" << endl;
-        }      
-};
-int main()
-{
-    Student s1;
-    s1.getDetails();
-    s1.showDetails();
-    s1.showGrade();
-    return 0;
-}
+//Defining a member function inside a class
+// #include <iostream>
+// #include <string>
+// using namespace std;
+// class Student
+// {
+// private:
+//     string name;
+//     int rollno;
+//     int marks;
+// public:
+//     void getDetails()
+//     {
+//         cout << "Enter full name of student: ";
+//         getline(cin, name);
+//         cout << "Enter roll number of student: ";
+//         cin >> rollno;
+//         cout << "Enter marks of student: ";
+//         cin >> marks;
+//     }
+//     void showDetails()
+//     {
+//         cout << "Name of student is: " << name << endl;
+//         cout << "Roll number of student is: " << rollno << endl;
+//         cout << "Marks of student is: " << marks << endl;
+//     }
+//     void showGrade()
+//     {
+//         if (marks >= 90)
+//             cout << "Grade of student is: A+" << endl;
+//         else if (marks >= 80 && marks < 90)
+//             cout << "Grade of student is: A" << endl;
+//         else if (marks >= 70 && marks < 80)
+//             cout << "Grade of student is: B" << endl;
+//         else if (marks >= 60 && marks < 70)
+//             cout << "Grade of student is: C" << endl;
+//         else if (marks >= 50 && marks < 60)
+//             cout << "Grade of student is: D" << endl;
+//         else
+//             cout << "Grade of student is: F" << endl;
+//     }
+// };
+// int main()
+// {
+//     Student s1;
+//     s1.getDetails();
+//     s1.showDetails();
+//     s1.showGrade();
+//     return 0;
+// }
+
+//wap to sum the function overloading in C++ and must sum output of each function
+// #include <iostream>
+// using namespace std;
+// class Overload
+// {
+// public:
+//     void sum(int a,int b)
+//     {
+//         cout << "The sum of a and b is: " << a + b << endl;
+//     }
+//     void sum(int a, double b) //function overloading
+//     {
+//         cout << "The sum of a and b is: " << a + b << endl;
+//     }
+//     void sum(int a, double b, double c) //function overloading
+//     {
+//         cout << "The sum of a, b and cis: " << a + b + c << endl;
+//     }
+// };
+// int main()
+// {
+//     Overload o1;
+//     o1.sum(5,5);         // calls the first function
+//     o1.sum(5,5.5);       // calls the second function
+//     o1.sum(5,10.0,5.5);    // calls the third function
+//     return 0;
+// }
+//write a simple program to show the operator for + operator which is used to add two numbers as well as two strings in C++
+// #include <iostream>
+// #include <string>
+// using namespace std;
+// class Overload
+// {
+// public:
+//     int sum(int a, int b)
+//     {
+//         return a + b;
+//     }
+//     string sum(string a, string b) //function overloading
+//     {
+//         return a + b;
+//     }
+// };
+// int main()
+// {
+//     Overload o1;
+//     cout << "The sum of 5 and 5 is: " << o1.sum(5, 5) << endl; // calls the first function
+//     cout << "The concatenated string is: " << o1.sum("Hello ", "World") << endl; // calls the second function
+//     return 0;
+// }
+
+//Defining a member function outside a class
+// # include <iostream>
+// # include <string>
+// using namespace std;
+// class Student
+// {
+// private:
+//     string name;
+//     int rollno;
+//     int marks;
+// public:
+//     void getDetails();
+//     void showDetails();
+// };
+// void Student::getDetails()
+// {
+//     cout << "Enter full name of student: ";
+//     getline(cin, name);
+//     cout << "Enter roll number of student: ";
+//     cin >> rollno;
+//     cout << "Enter marks of student: ";
+//     cin >> marks;
+// }
+// void Student::showDetails()
+// {
+//     cout << "Name of student is: " << name << endl;
+//     cout << "Roll number of student is: " << rollno << endl;
+//     cout << "Marks of student is: " << marks << endl;
+// }
+// int main()
+// {
+//     Student s1;
+//     s1.getDetails();
+//     s1.showDetails();
+//     return 0;
+// }
