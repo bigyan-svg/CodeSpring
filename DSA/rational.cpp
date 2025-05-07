4b8ba4141ce2e7f70aa503d655409db5806890e8
Bigyan Sanjyal <bigyansanjyal56@gmail.com>
2025-05-07 21:41:50 +0545

    modify

--- DSA/rational.cpp
+++ DSA/rational.cpp
@@ -1,42 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 class Rational
 {
-    private:
-    int num;
-    int den;
-    public:
-    Rational(int n=0, int d=1)
-    {
-        num = n;
-        den = d;
-    }
-    void input()
-    {
-        cout << "Enter numerator and denominator: ";
-        cin >> num >> den;
-    }
-    void display()
-    {
-        cout << num << "/" << den << endl;
-    }
-    Rational add(Rational r)
-    {
-        Rational number;
-        number.num= num*r.den+ den*r.num;
-        number.den= den*r.den;
-        return number;
-    }
+  private:
+      int num;
+      int den;
+  public:
+      Rational(int n=0,int d=1)
+      {
+          num = n;
+          den = d;
+      }
+      void input()
+      {
+          cout << "Enter numerator and denominator: ";
+          cin >> num >> den;
+      }
+      void display()
+      {
+          cout<<"sum = "<<num<<"/"<<den<<endl;
+      }
+     friend Rational add(Rational r1, Rational r2);
 };
-int main()
+Rational add(Rational r1, Rational r2)
 {
-    Rational r1, r2, result;
-    cout << "Enter first rational number:" << endl;
-    r1.input();
-    cout << "Enter second rational number:" << endl;
-    r2.input();
-    result = r1.add(r2);
-    cout << "Sum of the two rational numbers is: ";
-    result.display();
-    return 0;
+  Rational r;
+  r.num = r1.num*r2.den+r1.den*r2.num;
+  r.den = r1.den*r2.den;
+  return r;
+}
+int main() 
+{
+  Rational r1,r2,result;
+  cout<<"enter the first number: "<<endl;
+  r1.input();
+  cout<<"Enter the second number"<<endl;
+  r2.input();
+  result = add(r1, r2);
+  result.display();
+  return 0;
 }
