a27eb5a8d9cf70ef87efb24b481c097b72c799c9
Bigyan Sanjyal <bigyansanjyal56@gmail.com>
2025-05-25 21:38:00 +0545

    modifying code

--- DSA/Double_index_Queue.c
+++ DSA/Double_index_Queue.c
@@ -1,55 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define SIZE 5
+
 int queue[SIZE];
 int front = -1, rear = -1;
+
 void enqueue_Right(int value)
 {
     int i;
-    if((front==-1 && rear == SIZE-1)||(front == rear+1))
+    if ((front == 0 && rear == SIZE - 1))
     {
-        printf("sorry, Queue overflowed!\n");
+        printf("Sorry, queue overflowed!\n");
         return;
     }
-    if(front==-1 && rear==-1)
+
+    if (front == -1) // First element
     {
-        front = 0;
-        rear = 0;
-        queue[rear]=value;
+        front = rear = 0;
+        queue[rear] = value;
     }
-    else
+    else if (rear < SIZE - 1)
     {
-        if(rear!=SIZE-1) 
+        rear++;
+        queue[rear] = value;
+    }
+    else // Need to shift elements to the front
+    {
+        int j;
+        for (j = front; j <= rear; j++)
         {
-            rear++;
-            queue[rear]=value;
+            queue[j - front] = queue[j];
         }
-        else
-        {
-            for(i=front;i<=rear; i++)
-            {
-                queue[i-1]= queue[i];
-            }
-            front--;
-            queue[rear]=value;
-        }
+        rear = rear - front;
+        front = 0;
+        rear++;
+        queue[rear] = value;
     }
 }
+
 void display()
 {
     int i;
-    if(front ==-1 && rear ==-1)
+    if (front == -1)
     {
         printf("Queue is empty!\n");
         return;
     }
     printf("Queue:\n");
-    for(i=front; i<=rear; i++)
+    for (i = front; i <= rear; i++)
     {
-        printf("%d ",&queue[i]);
+        printf("%d ", queue[i]);
     }
     printf("\n");
 }
+
 int main()
 {
     int choice, value;
@@ -63,18 +68,18 @@ int main()
         scanf("%d", &choice);
         switch (choice)
         {
-            case 1:
-                printf("Enter the element to be inserted: ");
-                scanf("%d",&value);
-                enqueue_Right(value);
-                break;
-            case 2:
-                display();
-                break;
-            case 3:
-                exit(0);
-            default:
-                printf("Invalid choice\n");
+        case 1:
+            printf("Enter the element to be inserted: ");
+            scanf("%d", &value);
+            enqueue_Right(value);
+            break;
+        case 2:
+            display();
+            break;
+        case 3:
+            exit(0);
+        default:
+            printf("Invalid choice\n");
         }
     }
     return 0;
