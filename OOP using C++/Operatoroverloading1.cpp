40fa19ddaacbf6b7cb0f0a1bc76e3262924757ac
Bigyan Sanjyal <bigyansanjyal56@gmail.com>
2025-06-27 22:31:45 +0545

    modifying code

---
+++ OOP using C++/Operatoroverloading1.cpp
@@ -0,0 +1,56 @@
+//WAP to overload binary operator (+) and (-) and showing the statement c3 = c1+c2, c3 = c1-c2
+
+#include <iostream>
+using namespace std;
+
+class Complex {
+private:
+    float real;
+    float imag;
+
+public:
+    // Constructor
+    Complex(float r = 0, float i = 0) {
+        real = r;
+        imag = i;
+    }
+
+    // Overload + operator
+    Complex operator + (const Complex& obj) 
+    {
+        Complex temp;
+        temp.real = real + obj.real;
+        temp.imag = imag + obj.imag;
+        return temp;
+    }
+
+    // Overload - operator
+    Complex operator - (const Complex& obj) {
+        Complex temp;
+        temp.real = real - obj.real;
+        temp.imag = imag - obj.imag;
+        return temp;
+    }
+
+    // Display function
+    void display() {
+        cout<<"The complex number is: " << real << " + " << imag << "i" << endl;
+    }
+};
+
+// Main function
+int main() {
+    Complex c1(4, 6);
+    Complex c2(2, 1);
+    Complex c3;
+
+    // Addition
+    c3 = c1 + c2;
+    c3.display();
+
+    // Subtraction
+    c3 = c1 - c2;
+    c3.display();
+
+    return 0;
+}
